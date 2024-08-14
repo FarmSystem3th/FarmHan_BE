3ff53235f0902bf0c3fbc72798c392793bd4e439
ansungwoo <asw4ever@naver.com>
2024-08-14 14:58:31 +0900

    feat: Backend init 세팅

---
+++ src/main/java/is/farmhan/FarmhanApplication.java
@@ -0,0 +1,13 @@
+package is.farmhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmhanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/farmhan/FarmhanApplicationTests.java
@@ -0,0 +1,13 @@
+package is.farmhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
