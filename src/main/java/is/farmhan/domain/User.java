0e45f8ab2bb8f236fde077073a47ecea81b4e05e
ansungwoo <asw4ever@naver.com>
2024-08-20 13:58:13 +0900

    fix : user 버그 수정

--- src/main/java/is/farmhan/domain/User.java
+++ src/main/java/is/farmhan/domain/User.java
@@ -1,4 +1,14 @@
 package is.farmhan.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor
 public class User {
+    @Id
+    private Long id;
+
+
 }
