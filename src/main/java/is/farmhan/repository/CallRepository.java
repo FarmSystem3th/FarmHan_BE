629a31bfc49ef36720703abe1b2c318e6ca2f8e6
ansungwoo <asw4ever@naver.com>
2024-08-29 13:45:27 +0900

    fix: callHistoryList 최신순 정렬 추가

--- src/main/java/is/farmhan/repository/CallRepository.java
+++ src/main/java/is/farmhan/repository/CallRepository.java
@@ -3,13 +3,14 @@ package is.farmhan.repository;
 import is.farmhan.domain.Call;
 import is.farmhan.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CallRepository extends JpaRepository<Call, Long> {
-
+    @Query("SELECT c FROM Call c WHERE c.user.id = :userId ORDER BY c.createAt desc ")
     List<Call> findByUserId(Long userId);
 
 }
